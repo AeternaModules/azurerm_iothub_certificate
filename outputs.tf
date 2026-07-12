7a1a47ff9dcf090db4fb8dd1b0b950f9bbdffa81
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:31 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "iothub_certificates_id" {
+  description = "Map of id values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.id }
+}
 output "iothub_certificates_certificate_content" {
   description = "Map of certificate_content values across all iothub_certificates, keyed the same as var.iothub_certificates"
   value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.certificate_content }
