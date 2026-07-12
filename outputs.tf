ba4c45d27197cc3621d3c9a1aa9114be0b69d966
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,22 @@
-output "iothub_certificates" {
-  description = "All iothub_certificate resources"
-  value       = azurerm_iothub_certificate.iothub_certificates
-  sensitive   = true
-}
 output "iothub_certificates_certificate_content" {
-  description = "List of certificate_content values across all iothub_certificates"
-  value       = [for k, v in azurerm_iothub_certificate.iothub_certificates : v.certificate_content]
+  description = "Map of certificate_content values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.certificate_content }
   sensitive   = true
 }
 output "iothub_certificates_iothub_name" {
-  description = "List of iothub_name values across all iothub_certificates"
-  value       = [for k, v in azurerm_iothub_certificate.iothub_certificates : v.iothub_name]
+  description = "Map of iothub_name values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.iothub_name }
 }
 output "iothub_certificates_is_verified" {
-  description = "List of is_verified values across all iothub_certificates"
-  value       = [for k, v in azurerm_iothub_certificate.iothub_certificates : v.is_verified]
+  description = "Map of is_verified values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.is_verified }
 }
 output "iothub_certificates_name" {
-  description = "List of name values across all iothub_certificates"
-  value       = [for k, v in azurerm_iothub_certificate.iothub_certificates : v.name]
+  description = "Map of name values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.name }
 }
 output "iothub_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all iothub_certificates"
-  value       = [for k, v in azurerm_iothub_certificate.iothub_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all iothub_certificates, keyed the same as var.iothub_certificates"
+  value       = { for k, v in azurerm_iothub_certificate.iothub_certificates : k => v.resource_group_name }
 }
 
